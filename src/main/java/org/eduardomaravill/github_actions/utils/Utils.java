7441a2e14a28a0951c68e6b0cd43a9daea1eb4a6
Eduardo Maravilla <eduardomaravillag@gmail.com>
2025-06-16 16:45:11 -0600

    push with workflow

---
+++ src/main/java/org/eduardomaravill/github_actions/utils/Utils.java
@@ -0,0 +1,8 @@
+package org.eduardomaravill.github_actions.utils;
+
+public class Utils {
+
+    public static int getMinNumber(int a, int b) {
+        return Math.min(a, b);
+    }
+}
---
+++ src/test/java/org/eduardomaravill/github_actions/utils/UtilsTest.java
@@ -0,0 +1,17 @@
+package org.eduardomaravill.github_actions.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    void getMinNumber() {
+        assertEquals(1, Utils.getMinNumber(1, 2));
+        assertEquals(2, Utils.getMinNumber(2, 2));
+        assertEquals(-1, Utils.getMinNumber(-1, 0));
+        assertEquals(-5, Utils.getMinNumber(-5, -3));
+        assertEquals(0, Utils.getMinNumber(0, 0));
+    }
+}
