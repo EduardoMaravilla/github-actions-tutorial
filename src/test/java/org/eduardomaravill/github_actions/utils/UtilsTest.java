a5deca990b1a67b909097cdb69572908c8b1014c
Eduardo Maravilla <eduardomaravillag@gmail.com>
2025-06-17 12:04:18 -0600

    Modificacion del test

--- src/test/java/org/eduardomaravill/github_actions/utils/UtilsTest.java
+++ src/test/java/org/eduardomaravill/github_actions/utils/UtilsTest.java
@@ -10,7 +10,7 @@ class UtilsTest {
     void getMinNumber() {
         assertEquals(1, Utils.getMinNumber(1, 2));
         assertEquals(2, Utils.getMinNumber(2, 2));
-        assertEquals(-1, Utils.getMinNumber(-1, 0));
+        assertEquals(-5, Utils.getMinNumber(-1, 0));
         assertEquals(-5, Utils.getMinNumber(-5, -3));
         assertEquals(0, Utils.getMinNumber(0, 0));
     }
